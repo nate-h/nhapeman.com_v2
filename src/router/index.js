0b61bdea0804bb5e5c4b25bb88be8b6c75f5c9d3
nate <nhapeman@gmail.com>
2018-02-19 22:00:59 -0800

    add test component and make default route that component.

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Test from '@/components/Test'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      path: '/test',
+      name: 'Test',
+      component: Test
+    },
+    {
       path: '/',
+      name: 'Test',
+      component: Test
+    },
+    {
+      path: '/help',
       name: 'HelloWorld',
       component: HelloWorld
     }
+
   ]
 })
