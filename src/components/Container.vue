89579109828a6df0ad81c3bd9a6e03687937758e
nates linux desktop <nhapeman@appliedinvention.com>
2018-03-18 23:24:44 -0700

    start container directive

---
+++ src/components/Container.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Container',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    .container {
+        box-shadow: 0 4px 6px 0 $shadow;
+    }
+</style>
